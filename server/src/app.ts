a0f2b2d4b2fd77345e5433de61028ff44a3c12ad
HeavenProx <92014737+HeavenProx@users.noreply.github.com>
2026-05-25 15:34:59 +0200

    feat: traefik + portainer

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,6 +9,11 @@ app.use(express.json());
 // use the router to answers request on /api
 app.use("/api", apiRouter);
 
+// hello endpoint
+app.get("/", (_, res) => {
+  res.send("Bonjour !");
+});
+
 // return HTTP 404 if the request has not been handled
 app.use((_, res) => {
   res.status(404).send();
